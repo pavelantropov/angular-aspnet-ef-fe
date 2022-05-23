618f377c967e3608687ccdb88cf781b0e4769139
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-23 23:16:33 +0400

    Create an empty component

---
+++ src/app/components/question/question.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "question",
+  template: "A question",
+})
+class QuestionComponent {
+
+}
+
+export default QuestionComponent;
